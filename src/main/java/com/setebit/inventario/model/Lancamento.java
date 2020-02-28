4f0e7c357cdf0fb835768ebcb4cec61ffec9b85f
claudemir ramos ferreira <claudemirramosferreira@gmail.com>
2020-02-28 11:54:00 -0400

    incluir o construtor

--- src/main/java/com/setebit/inventario/model/Lancamento.java
+++ src/main/java/com/setebit/inventario/model/Lancamento.java
@@ -63,4 +63,39 @@ public class Lancamento extends AbstractEntity implements Serializable {
 		this.contagem = contagem;
 	}
 
+	public Lancamento() {
+	}
+
+	public Lancamento(Integer id, Float quantidade, String observacao, Contagem contagem) {
+		this.id = id;
+		this.quantidade = quantidade;
+		this.observacao = observacao;
+		this.contagem = contagem;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lancamento other = (Lancamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
