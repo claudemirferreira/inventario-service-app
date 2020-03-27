2e8390764f30eab12d026e7ba47ec55a06813ac7
claudemir <claudemirramosferreira@gmail.com>
2020-03-27 01:58:37 -0400

    continuacao

--- src/main/java/com/setebit/inventario/resource/AbstractController.java
+++ src/main/java/com/setebit/inventario/resource/AbstractController.java
@@ -1,103 +1,57 @@
 package com.setebit.inventario.resource;
 
-import java.io.Serializable;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.util.Collection;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.setebit.inventario.model.AbstractEntity;
 import com.setebit.inventario.response.Response;
-import com.setebit.inventario.service.IService;
+import com.setebit.inventario.service.BaseService;
+import com.setebit.inventario.util.ObjectMapperUtils;
 
-public abstract class AbstractController<T extends AbstractEntity> implements Serializable {
+/**
+ *
+ * @param <T>   source Entity
+ * @param <ID>  Entity's id
+ * @param <DTO> DTO object to convert
+ */
+public abstract class AbstractMapperController<T, ID, DTO> {
 
-	private static final long serialVersionUID = 1L;
+	protected abstract BaseService<T, ID> getService();
 
-	/** Retornar o serviço da entidade. */
-	protected abstract IService<T, Integer> getService();
+	protected abstract Class<DTO> getDtoClass();
 
-	@GetMapping()
-	public ResponseEntity<Response<List<T>>> find() {
-		Response<List<T>> response = new Response<List<T>>();
-		response.setData(getService().listAll());
+	@RequestMapping(value = "", method = GET, produces = APPLICATION_JSON_VALUE)
+	public ResponseEntity<Response<List<DTO>>> find() {
+		Response<List<DTO>> response = new Response<List<DTO>>();
+		response.setData(toDto(getService().getAll()));
 		return ResponseEntity.ok(response);
 	}
 
-	@PostMapping()
-	public ResponseEntity<Response<T>> create(HttpServletRequest request, @RequestBody T entity, BindingResult result) {
-		Response<T> response = new Response<T>();
+	@RequestMapping(value = "/{id}", method = GET, produces = APPLICATION_JSON_VALUE)
+	public ResponseEntity<Response<DTO>> findById(@PathVariable("id") ID id) {
+		Response<DTO> response = new Response<DTO>();
 		try {
-			validateUpdate(entity, result);
-			if (result.hasErrors()) {
-				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
-				return ResponseEntity.badRequest().body(response);
-			}
-			entity = getService().save(entity);
-			response.setData(entity);
-		} catch (DuplicateKeyException dE) {
-			response.getErrors().add("E-mail already registered !");
-			return ResponseEntity.badRequest().body(response);
+			DTO dto = toDto(getService().getById(id));
+			response.setData(dto);
+			return ResponseEntity.ok(response);
 		} catch (Exception e) {
-			response.getErrors().add(e.getMessage());
-			return ResponseEntity.badRequest().body(response);
-		}
-		return ResponseEntity.ok(response);
-	}
-
-	@PutMapping()
-	public ResponseEntity<Response<T>> update(HttpServletRequest request, @RequestBody T entity, BindingResult result) {
-		Response<T> response = new Response<T>();
-		try {
-			validateUpdate(entity, result);
-			if (result.hasErrors()) {
-				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
-				return ResponseEntity.badRequest().body(response);
-			}
-			T userPersisted = (T) getService().save(entity);
-			response.setData(userPersisted);
-		} catch (Exception e) {
-			response.getErrors().add(e.getMessage());
-			return ResponseEntity.badRequest().body(response);
-		}
-		return ResponseEntity.ok(response);
-	}
-
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Response<String>> delete(@PathVariable("id") Integer id) {
-		Response<String> response = new Response<String>();
-		T user = getService().findOne(id);
-		if (user == null) {
 			response.getErrors().add("Register not found id:" + id);
 			return ResponseEntity.badRequest().body(response);
 		}
-		getService().deleteById(id);
-		return ResponseEntity.ok(new Response<String>());
 	}
 
-	@GetMapping(value = "{id}")
-	public ResponseEntity<Response<T>> findById(@PathVariable("id") Integer id) {
-		Response<T> response = new Response<T>();
-		T user = getService().findOne(id);
-		if (user == null) {
-			response.getErrors().add("Register not found id:" + id);
-			return ResponseEntity.badRequest().body(response);
-		}
-		response.setData(user);
-		return ResponseEntity.ok(response);
+	private List<DTO> toDto(Collection<T> entityList) {
+		return ObjectMapperUtils.mapAll(entityList, getDtoClass());
 	}
 
-	public abstract void validateCreate(T entity, BindingResult result);
-
-	public abstract void validateUpdate(T entity, BindingResult result);
+	private DTO toDto(T entityList) {
+		return ObjectMapperUtils.map(entityList, getDtoClass());
+	}
 
 }
